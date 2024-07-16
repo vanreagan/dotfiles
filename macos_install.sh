03ed3759b2e9ca3037baa36e7a0fcced96469924
vanreagan <103523086+vanreagan@users.noreply.github.com>
2024-07-16 16:00:27 -0600

    Format update on macos_install

--- macos_install.sh
+++ macos_install.sh
@@ -1,6 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-# Create symlinks for dotfiles
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Create symlinks for dotfiles, assuming the dotfiles are in the same directory as the script
+ln -s "$SCRIPT_DIR/.bash_profile" ~/.bash_profile
+ln -s "$SCRIPT_DIR/.gitconfig" ~/.gitconfig
+ln -s "$SCRIPT_DIR/.bashrc" ~/.bashrc
