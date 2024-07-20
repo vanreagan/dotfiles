314f798dc2959879c50c279aa7dc77773684ea18
vanreagan <103523086+vanreagan@users.noreply.github.com>
2024-07-19 19:24:54 -0600

    Remove cheesy formating in windows install, allow for script to be run as admin and still have the option to select the install user

--- windows_install.cmd
+++ windows_install.cmd
@@ -1,34 +1,37 @@
 @echo off
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: H E A D E R
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: File:        windows_install.cmd
-:: Creator:     vanreagan
-:: Date:        18/07/2024
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@REM * Config
+
 setlocal enableextensions enabledelayedexpansion
 set me=%~n0
 set parent=%~dp0
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: C O N F I G U R A T I O N
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@REM Check if a username is provided as a command-line argument because this script needs to be run as admin, and the user directory is not the same as the admin directory in all cases.
+if "%~1"=="" (
+    @REM Prompt the user for a username
+    set /p "USERNAME=Enter the username for installation (press Enter for admin user): "
+    if "!USERNAME!"=="" (
+        set "USER_LOCALAPPDATA=%LOCALAPPDATA%"
+    ) else (
+        @REM Assuming C:\Users as the user directory. 
+        set "USER_LOCALAPPDATA=C:\Users\!USERNAME!\AppData\Local"
+    )
+) else (
+    @REM Use the provided command-line argument as the username
+    set "USER_LOCALAPPDATA=C:\Users\%~1\AppData\Local"
+)
 
 set "SOURCE_PATH=%parent%settings.json"
-set "TARGET_PATH=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+set "TARGET_PATH=%USER_LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: M A I N
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@REM * Main
 :start
-	@REM Delete the existing settings.json if it exists to avoid conflict
-	if exist "%TARGET_PATH%" del "%TARGET_PATH%"
+    @REM Delete the existing settings.json if it exists to avoid conflict
+    if exist "%TARGET_PATH%" del "%TARGET_PATH%"
 
-	@REM Create a symbolic link to the settings.json file
-	mklink "%TARGET_PATH%" "%SOURCE_PATH%"
+    @REM Create a symbolic link to the settings.json file
+    mklink "%TARGET_PATH%" "%SOURCE_PATH%"
 
 :end
-	pause
-	endlocal
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: F U N C T I O N S
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    pause
+    endlocal
