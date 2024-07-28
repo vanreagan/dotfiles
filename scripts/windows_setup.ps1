9514636e890898210776cbae9666663267d933cf
vanreagan <103523086+vanreagan@users.noreply.github.com>
2024-07-27 22:42:06 -0600

    Refactor windows setup script, improve organization and add comments

--- scripts/windows_setup.ps1
+++ scripts/windows_setup.ps1
@@ -1,42 +1,91 @@
-# [Windows]
 
-# Manual
-# 	Install winget in case it isn't already
-# 	Install Git
-# 	Clone dotfiles repo
+# Check if winget is installed
+$wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
 
-# Script should do:
+# If winget is not installed prompt the user to install it and exit the script
 
-# 	Install Windows Terminal X
-# 	Install New Powershell X
-# 	Install WSL2 X
-# 	Install Nerdfont X
-# 	Install VSCode X
-# 	Create symlink for Windows Terminal settings.json X
-# 	Create symlinks for gitconfig X
+if (-not $wingetInstalled) {
+	Write-Host "winget is not installed. Please install winget and run the script again."
+	Write-Host "You can install winget from https://apps.microsoft.com/detail/9nblggh4nns1"
 
-# TODO: Create a function for the symlink bs
-# TODO: Actually test the winget stuff
-# TODO: Write a loop which installs/checks for updates for all the programs in an array of strings
-# TODO: Check for winget presence and install it if it isn't there
-# TODO: Check for git presence and install it if it isn't there
-# TODO: Modify nerdfont installation function to take the userpath or something
+	# Exit the script
+	Exit
+}
+
+
+
+
+# * Main Script
+
+# Use the function to get a valid username
+$username = GetUser
+
+# Set the local appdata directory for the selected user.
+$userLocalAppData = "C:\Users\$username\AppData\Local"
+
+# Set the user profile directory
+$userProfile = "C:\Users\$username\"
+
+# Get the full path of the currently running script
+$scriptPath = $MyInvocation.MyCommand.Path
+
+Write-Output $scriptPath
+
+# Get the parent directory of the script
+$parent = $scriptPath.replace("\scripts\windows_setup.ps1", "")
+
+# Set the config directory
+$configPath = Join-Path $parent "config"
+
+# Continue with the rest of the script using the $username
+Write-Host "Proceeding with installation for user: $username"
+
+$programList = [
+	"Microsoft.WindowsTerminal",
+	"Microsoft.Powershell",
+	"Microsoft.VisualStudioCode",
+	"Git.Git",
+]
+
+InstallWingetList $programList
+
+# Fonts
+
+Write-Host "Installing Nerd Fonts"
+
+InstallNerdFont "Hack" "v3.2.1"
+
+
+# # Create symlinks
+$wtLocalState = Join-Path $userLocalAppData "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+CreateSymlink "settings.json" $wtLocalState
+
+CreateSymlink ".gitconfig" $userProfile
+
+
+
+# # Install WSL2
+# Check if WSL is already installed by listing installed distributions
+$wslDistributions = wsl --list --quiet
+if ($wslDistributions) {
+    Write-Host "WSL is already installed. Installed distributions:"
+    Write-Host $wslDistributions
+} else {
+    Write-Host "Installing WSL2..."
+    wsl --install
+}
 
 # * Functions
 
-function CheckWingetInstallation {
+function InstallWithWinget {
 	param(
 		[string]$programId
 	)
 
-	$installed = winget --list --id $programId
-	if ($installed -match $programId) {
-		Write-Host "$programId is already installed. Checking for updates..."
-		winget upgrade --id $programId --source winget
-	} else {
-		Write-Host "Installing $programId"
-		winget install --id $programId --source winget
-	}
+	Write-Host "Attempting to install $programId"
+	winget install --id=$programId --source winget -e
+
 }
 
 function InstallNerdFont {
@@ -46,8 +95,8 @@ function InstallNerdFont {
 	)
 
 	$nerdFontUrl = "https://github.com/ryanoasis/nerd-fonts/releases/download/$nerdFontVersion/$nerdFontName.zip"
-	$fontZipPath = "$env:USERPROFILE\Downloads\$nerdFontName.zip"
-	$fontExtractPath = "$env:USERPROFILE\Downloads\$nerdFontName"
+	$fontZipPath = "$userProfile\Downloads\$nerdFontName.zip"
+	$fontExtractPath = "$userProfile\Downloads\$nerdFontName"
 
 	Write-Host "Downloading Nerd Font..."
 	Invoke-WebRequest -Uri $nerdFontUrl -OutFile $fontZipPath
@@ -101,92 +150,33 @@ function GetUser {
 	return $username
 }
 
-# * Main Script
+function CreateSymlink {
+	param(
+		[string]$fileName,
+		[string]$targetDirectory
+	)
 
-# Use the function to get a valid username
-$username = GetUser
+	$sourcePath = Join-Path $configPath $fileName
+	$targetPath = Join-Path $targetDirectory $fileName
 
-# Set the local appdata directory for the selected user.
-$userLocalAppData = "C:\Users\$username\AppData\Local"
+	# If the file already exists, rename it as a backup.
+	if (Test-Path $targetPath) {
+		$backupPath = Join-Path $targetDirectory "$fileName.bak"
+		Move-Item -Path $targetPath -Destination $backupPath
+	}
 
-# Set the user profile directory
-$userProfile = "C:\Users\$username\"
+	# Create a symbolic link to the file
+	New-Item -ItemType SymbolicLink -Path $targetPath -Target $sourcePath
 
-# Get the full path of the currently running script
-$scriptPath = $MyInvocation.MyCommand.Path
-
-Write-Output $scriptPath
-
-# Get the parent directory of the script
-$parent = $scriptPath.replace("\scripts\windows_setup.ps1", "")
-
-# Set the config directory
-$configPath = Join-Path $parent "config"
-
-# Continue with the rest of the script using the $username
-Write-Host "Proceeding with installation for user: $username"
-# The rest of your script follows here, using $username where necessary
-
-# Write-Host "Starting Windows Setup..."
-
-# Write-Host "Installing Windows Terminal"
-
-# CheckWingetInstallation "Microsoft.WindowsTerminal"
-
-# Write-Host "Installing New Microsoft PowerShell"
-
-# CheckWingetInstallation "Microsoft.Powershell"
-
-# Write-Host "Installing Visual Studio Code"
-
-# CheckWingetInstallation "Microsoft.VisualStudioCode"
-
-# Fonts
-
-# Write-Host "Installing Nerd Fonts"
-
-# InstallNerdFont "Hack" "v3.2.1"
-
-
-# # Create symlink for Windows Terminal settings.json
-Write-Host "Creating symlink for Windows Terminal settings.json..."
-
-$wtSourcePath = Join-Path $configPath "settings.json"
-$wtTargetPath = Join-Path $userLocalAppData "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-# Delete the existing settings.json if it exists to avoid conflict
-if (Test-Path $wtTargetPath) {
-	# TODO: Should probably just create a .bak file instead of deleting 
-    Remove-Item $wtTargetPath
+	Write-Host "Created symlink for $fileName"
 }
 
-# Create a symbolic link to the settings.json file
-New-Item -ItemType SymbolicLink -Path $wtTargetPath -Target $wtSourcePath
+function InstallWingetList {
+	param(
+		[string[]]$programs
+	)
 
-
-
-# Create symlink for .gitconfig
-Write-Host "Creating symlink for .gitconfig..."
-
-$gcSourcePath = Join-Path $configPath ".gitconfig"
-$gcTargetPath = Join-Path $userProfile ".gitconfig"
-
-# Delete the existing .gitconfig if it exists to avoid conflict
-if (Test-Path $gcTargetPath) {
-		# TODO: Should probably just create a .bak file instead of deleting 
-		Remove-Item $gcTargetPath
+	foreach ($program in $programs) {
+		InstallWithWinget $program
+	}
 }
-
-# Create a symbolic link to the .gitconfig file
-New-Item -ItemType SymbolicLink -Path $gcTargetPath -Target $gcSourcePath
-
-# # Install WSL2
-# # Check if WSL is already installed by listing installed distributions
-# $wslDistributions = wsl --list --quiet
-# if ($wslDistributions) {
-#     Write-Host "WSL is already installed. Installed distributions:"
-#     Write-Host $wslDistributions
-# } else {
-#     Write-Host "Installing WSL2..."
-#     wsl --install
-# }
